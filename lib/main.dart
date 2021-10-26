0739ccb9bf8c678df52becef29ddfc619f849866
kazkura <kz3maz@gmail.com>
2021-10-26 17:27:46 +0900

    Modify: change TextField to TextFormField to add its validator.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _textEditingController = new TextEditingController();
   final _focusNode = new FocusNode();
+  final _testForm = GlobalKey<FormState>();
 
   String _numStr = '';
   String _message = '';
@@ -52,10 +53,15 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // 判定ボタンを押すと、入力した数字を判定関数に渡す
-  void _startJudge(){
-    setState(() {
-      _judgeNumber(_numStr);
-    });
+  void _startJudge(String value){
+    if(_testForm.currentState!.validate()) {
+      setState(() {
+        _judgeNumber(value);
+      });
+    }else{
+      _textEditingController.clear();
+      _focusNode.requestFocus();
+    }
   }
 
   // 判定してメッセージを表示する。ヒストリーに入力値を追加する
@@ -93,6 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _history = '';
       _endFlag = false;
     });
+    _focusNode.requestFocus();
   }
 
   // ウェジットのビルド関数
@@ -120,23 +127,44 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Container(
                   width: 200,
-                  child: Text('予想を入力してください: ')),
-                Container(
-                  width: 100,
-                  child: TextField(
-                    onChanged: _setNumber,
-                    onSubmitted: _judgeNumber,
-                    controller: _textEditingController,
-                    focusNode: _focusNode,
-                  )
+                  child: Text('予想を入力してください: ')
                 ),
+                Form(
+                  key: _testForm,
+                  child:
+                    Container(
+                        width: 200,
+                        child: TextFormField(
+                          controller: _textEditingController,
+                          focusNode: _focusNode,
+                          onChanged: _setNumber,
+                          onFieldSubmitted: (value) {
+                            _startJudge(value);
+                          },
+                          validator: (value) {
+                            if(value == null || value.isEmpty){
+                              return '何か数字を入力してください';
+                            }else{
+                              if( int.parse(value) < 1 || int.parse(value) > 100){
+                                return '1-100の間で数字を入力してください';
+                              }else{
+                                return null;
+                              }
+                            }
+                          },
+                        )
+                    ),
+                ),
+
                 Container(
                   width: 100,
                   child: TextButton(
-                    onPressed: _startJudge,
-                    child: Text('予想を入力')
+                    onPressed: () {
+                      _startJudge(_numStr);
+                      },
+                      child: Text('予想を入力')
+                    )
                   )
-                )
               ]
             ),
             // 判定結果
