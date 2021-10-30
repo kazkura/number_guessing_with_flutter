45f8579e14e32c0bf66abc7b155b93cfe518436f
kazkura <kz3maz@gmail.com>
2021-10-30 22:52:37 +0900

    modify: ファイルを分割

---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textEditingController = new TextEditingController();
+  final _focusNode = new FocusNode();
+  final _testForm = GlobalKey<FormState>();
+
+  String _numStr = '';
+  String _message = '';
+  String _history = '';
+  int _randNum = 0;
+  int _count = 0;
+  bool _endFlag = false;
+
+  // 初期化
+  @override
+  void initState() {
+    super.initState();
+    _randNum = Random().nextInt(100);
+  }
+
+  // テキストフィールドが更新されるたびに文字列をセット
+  void _setNumber(String text){
+    setState(() {
+      _numStr = text;
+    });
+  }
+
+  // 判定ボタンを押すと、入力した数字を判定関数に渡す
+  void _startJudge(String value){
+    if(_testForm.currentState!.validate()) {
+      setState(() {
+        _judgeNumber(value);
+      });
+    }else{
+      _textEditingController.clear();
+      _focusNode.requestFocus();
+    }
+  }
+
+  // 判定してメッセージを表示する。ヒストリーに入力値を追加する
+  void _judgeNumber(String numString){
+    _textEditingController.clear();
+    setState(() {
+      var num = int.parse(numString);
+      if(_endFlag == false){
+        if(_count < 9) {
+          if (num == _randNum) {
+            _message = 'おめでとう! 正解です!';
+          } else if (num > _randNum) {
+            _message = '今の予想は大きすぎです!もっと小さな数字です。';
+          } else {
+            _message = '今の予想は小さすぎです!もっと大きな数字です。';
+          }
+          _count += 1;
+        }else{
+          _message = 'ゲームオーバー';
+          _endFlag = true;
+        }
+        _history += num.toString() + ' ';
+      }
+      _focusNode.requestFocus();
+    });
+  }
+
+  // リセットする
+  void _resetGame(){
+    _textEditingController.clear();
+    setState(() {
+      _randNum = Random().nextInt(100);
+      _count = 0;
+      _message = '';
+      _history = '';
+      _endFlag = false;
+    });
+    _focusNode.requestFocus();
+  }
+
+  // ウェジットのビルド関数
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 説明文
+            Container(
+                width: 400.0,
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.lightBlue,
+                alignment: Alignment.center,
+                child: Text('1 から 100 までの数字を当ててみて！'
+                    '10 回以内に当てられるでしょうか。'
+                    '選んだ数字が大きいか小さいかを表示します。',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                  ),
+
+                )
+            ),
+            // 数値入力フォーム
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Form(
+                    key: _testForm,
+                    child:
+                    Container(
+                        width: 200,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: '予想する数字をいれてね',
+                          ),
+                          controller: _textEditingController,
+                          focusNode: _focusNode,
+                          onChanged: _setNumber,
+                          onFieldSubmitted: (value) {
+                            _startJudge(value);
+                          },
+                          validator: (value) {
+                            if(value == null || value.isEmpty){
+                              return '何か数字を入力してください';
+                            }else{
+                              if( int.tryParse(value) == null){
+                                return '自然数を入力してください';
+                              }else if( int.parse(value) < 1 || int.parse(value) > 100){
+                                return '1-100の間で数字を入力してください';
+                              }else{
+                                return null;
+                              }
+                            }
+                          },
+                        )
+                    ),
+                  ),
+
+                  Container(
+                      width: 100,
+                      child: TextButton(
+                          onPressed: () {
+                            _startJudge(_numStr);
+                          },
+                          child: Text('予想を入力')
+                      )
+                  )
+                ]
+            ),
+            // 判定結果
+            Container(
+                width: 400,
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.center,
+                child: Text('$_message')),
+            // 履歴
+            Container(
+                width: 400,
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.center,
+                child: Text('$_history')),
+            // リセットボタン
+            Container(
+                width: 400,
+                padding: EdgeInsets.all(10.0),
+                child: TextButton(
+                    onPressed: _resetGame,
+                    child: Text('再度挑戦')
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
